a510e0ddd978718ef3dcf5f3ddc45ba96089b372
itoht2 <itoht2@gmail.com>
2016-05-12 23:01:15 +0900

    他のが開いててもOKになった

---
+++ Assets/Scripts/MenuFlag.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFlag : MonoBehaviour {
+
+     public bool OtherMenuOpened;
+     public Animator _OpendMenuAnimation;
+
+	// Use this for initialization
+	void Start () {
+          OtherMenuOpened = false;
+          _OpendMenuAnimation = null;
+
+     }
+
+     // Update is called once per frame
+     void Update () {
+	
+	}
+
+     public bool GetOtherMenuOpend()
+     {
+          return OtherMenuOpened;
+     }
+}
--- Assets/Scripts/MenuOpener.cs
+++ Assets/Scripts/MenuOpener.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class MenuOpener : MonoBehaviour {
+     public Animator _menuAnim;
+     public MenuFlag Menuflag;
+     private Animator _OtherAnim;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+          _menuAnim = gameObject.GetComponent<Animator>();
+     }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,7 +18,28 @@ public class MenuOpener : MonoBehaviour {
 
      public void ControlMenuAnimator()
      {
-          Animator _menuAnim = gameObject.GetComponent<Animator>();
+         //_menuAnim = gameObject.GetComponent<Animator>();
+
+          if (Menuflag.OtherMenuOpened )
+          {
+               _OtherAnim = Menuflag._OpendMenuAnimation;
+               if (_OtherAnim != _menuAnim )
+               {
+                    _OtherAnim.SetBool("UP", !_OtherAnim.GetBool("UP"));
+                    Menuflag.OtherMenuOpened = true;
+
+               } else
+               {                    
+                    Menuflag.OtherMenuOpened = false;
+               }              
+
+          } else
+          {
+               Menuflag.OtherMenuOpened = true;              
+          }
+
           _menuAnim.SetBool("UP", !_menuAnim.GetBool("UP"));
+          Menuflag._OpendMenuAnimation = _menuAnim;
+         
      }
 }
