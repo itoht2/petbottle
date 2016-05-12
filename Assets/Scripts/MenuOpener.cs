46eb619888da2c4f06641655b5c11a7e34d2cf10
itoht2 <itoht2@gmail.com>
2016-05-12 14:20:51 +0900

    menuなんとか動くようになったかも

---
+++ Assets/Scripts/MenuOpener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuOpener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+     public void ControlMenuAnimator()
+     {
+          Animator _menuAnim = gameObject.GetComponent<Animator>();
+          _menuAnim.SetBool("UP", !_menuAnim.GetBool("UP"));
+     }
+}
