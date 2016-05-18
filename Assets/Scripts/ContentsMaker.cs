13f46144f84be62dd0d8938de1311c9d20762e75
itoht2 <itoht2@gmail.com>
2016-05-18 15:11:31 +0900

    進める

--- Assets/Scripts/ContentsMaker.cs
+++ Assets/Scripts/ContentsMaker.cs
@@ -1,22 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ContentsMaker : MonoBehaviour {
      public GameObject nodeFrefab;
      public NoseCornFolder noseCornFolder;
+     public GameObject content;
      private int numberOfItem;
-
-	// Use this for initialization
-	void Start () {
+     private Text Disc;
+     
+     // Use this for initialization
+     void Start()
+     {
 
           numberOfItem = noseCornFolder.GetNumberOfItem();
 
-          for (int i =0; i < numberOfItem; i++)
+          for (int i = 0; i < numberOfItem; i++)
           {
-               Debug.Log("i=" + i);
+               //Debug.Log("i=" + i);
+               GameObject Item = (GameObject)Instantiate(
+                    nodeFrefab,
+                    transform.position,
+                    Quaternion.identity
+                    );
+
+               Item.transform.SetParent(content.transform);
+               Item.transform.localScale = new Vector3(1, 1, 1);
+               Item.name = "Item" + i;
+               Disc = Item.transform.Find("Description").GetComponent<Text>();
+               Disc.text = noseCornFolder.GetDiscription(i);
+
           }
-	
-	}
+     }
 	
 	// Update is called once per frame
 	void Update () {
