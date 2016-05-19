aa6c0da59bb25c4a818cd583627d2c71f99c91a1
itoht2 <itoht2@gmail.com>
2016-05-19 17:15:42 +0900

    詳細ダイアログをアニメ化

---
+++ Assets/Scripts/DialogOpener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogOpener : MonoBehaviour {
+     public Animator _menuAnim;
+
+     // Use this for initialization
+     void Start () {
+          _menuAnim = gameObject.GetComponent<Animator>();
+     }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+     public void ControlMenuAnimator()
+     {
+          _menuAnim.SetBool("UP", !_menuAnim.GetBool("UP"));
+
+     }
+}
---
+++ Assets/Scripts/OpenDeatilButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class OpenDeatilButton : MonoBehaviour {
+
+     public GameObject detailDialog;
+
+
+	// Use this for initialization
+	void Start () {
+         
+     }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+     void OpenDialog()
+     {
+
+     }
+
+     void CloseDialog ()
+     {
+
+     }
+}
