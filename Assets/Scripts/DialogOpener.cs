5340904a08da747739ba76c71361df21b720d01c
itoht2 <itoht2@gmail.com>
2016-07-10 22:19:49 +0900

    SRBA本数のダイアログ追加

--- Assets/Scripts/DialogOpener.cs
+++ Assets/Scripts/DialogOpener.cs
@@ -13,6 +13,7 @@ public class DialogOpener : MonoBehaviour {
      public int SelectedNumber;
      private Text priceText;
      private Text specText;
+     private GameObject SRBANumber;
 
 
      // Use this for initialization
@@ -21,13 +22,13 @@ public class DialogOpener : MonoBehaviour {
 
           Content = this.transform.parent.gameObject;
           //Debug.Log(Content.name);
+
+          SRBANumber = GameObject.Find("SRBAText");
+
           if (Content.name !="Canvas")
           { 
                _noseCornFolder = Content.GetComponent<ContentsMaker>().GetNoseCornFolder();
-          
-          
-
-          _menuAnim = detailDialog.GetComponent<Animator>();
+               _menuAnim = detailDialog.GetComponent<Animator>();
 
           //int.TryParse(this.name.Substring(4, 1), out SelectedNumber);
           //int.TryParse(this.name.Substring(this.name.Length - 1, 1), out SelectedNumber);
@@ -77,6 +78,21 @@ public class DialogOpener : MonoBehaviour {
 
                int nowUsed = _noseCornFolder.GetNowUsed();
                Text NowUsingText = detailDialog.transform.FindChild("NowUsingText").GetComponent<Text>();
+
+               Debug.Log(_noseCornFolder.name.Substring(0, 4));
+               if (_noseCornFolder.name.Substring(0,4) == "SRBA")
+               {
+                    SRBANumber.SetActive(true);
+                    Text SRBAText =  SRBANumber.GetComponent<Text>();
+                    SRBAText.text = _noseCornFolder.GetParameterValue1(SelectedNumber) + "本";
+
+               }
+               else
+               {
+                    SRBANumber.SetActive(false);
+               }
+
+
                if (nowUsed == SelectedNumber)
                {
                     NowUsingText.enabled = true;
